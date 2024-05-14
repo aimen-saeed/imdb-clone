c3f30903d88f4ef538d81d73a2e61c25d47d14de
aimen-saeed <aimen.saeed20@gmail.com>
2024-05-14 19:34:54 +0000

    created about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,39 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl text-amber-500 font-medium">About</h1>
+      <p>
+        Welcome to IMDb, your ultimate destination for everything related to
+        movies and television! At IMDb, we're passionate about providing movie
+        buffs and TV enthusiasts with a comprehensive platform to explore,
+        discover, and engage with their favorite films and shows. Whether you're
+        looking for in-depth information about your favorite actors and
+        directors, seeking recommendations for your next binge-watch session, or
+        eager to dive into the latest industry news and gossip, IMDb has got you
+        covered.
+      </p>
+      <p>
+        Our mission at IMDb is to create a vibrant and inclusive community where
+        cinephiles from all walks of life can come together to celebrate the art
+        of storytelling through film and television. From classic masterpieces
+        to cutting-edge indie gems, we strive to offer a diverse range of
+        content to cater to every taste and preference. With our user-friendly
+        interface and intuitive search functionality, navigating through our
+        extensive database of titles has never been easier. Whether you're a
+        casual viewer or a hardcore film aficionado, IMDbatics is the perfect
+        place to indulge your passion for all things cinema.
+      </p>
+      <p>
+        Join us at IMDb and embark on an exciting journey through the
+        mesmerizing world of movies and television. With our user-generated
+        ratings and reviews, personalized recommendations, and exclusive
+        behind-the-scenes content, you'll never run out of things to explore and
+        discover. Whether you're craving the thrill of a blockbuster action
+        flick, the emotional depth of a heartfelt drama, or the
+        laughter-inducing antics of a comedy series, IMDb is your go-to
+        destination for all things entertainment. So sit back, relax, and let
+        the magic of cinema sweep you away!
+      </p>
+    </div>
+  );
+}
